a53f3def138fb903b7a080c079ae763256dc4508
ShreyasHakke1809 <shreyashakke5858@gmail.com>
2023-07-04 16:52:21 +0530

    Modifying program.cs file to find employee attendence

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            const int IS_PRESENT = 1;
+            Console.WriteLine("Welcome To EmployeeWageComputation");
+            Random random = new Random();
+            int employeeInput=random.Next(0,2);
+            Console.WriteLine("Random value :{0}", employeeInput);
+            if (employeeInput==IS_PRESENT) 
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            Console.ReadLine();
         }
     }
 }
