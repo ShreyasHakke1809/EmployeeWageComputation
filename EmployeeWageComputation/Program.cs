f13bfd45fc9c948051eb7fbde1fbdb0d17d1bf78
ShreyasHakke1809 <shreyashakke5858@gmail.com>
2023-07-04 17:15:08 +0530

    Modifying program.cs file to calculate employees daily wage

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             const int IS_PRESENT = 1;
+            const int EMP_RATE_PER_HR = 20;
+            int empHrs = 0;
+            int empWage = 0;
             Console.WriteLine("Welcome To EmployeeWageComputation");
             Random random = new Random();
             int employeeInput=random.Next(0,2);
@@ -12,11 +15,14 @@
             if (employeeInput==IS_PRESENT) 
             {
                 Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
             }
+            empWage = empHrs * EMP_RATE_PER_HR;
+            Console.WriteLine("Daily employee wage is :{0}", empWage);
             Console.ReadLine();
         }
     }
