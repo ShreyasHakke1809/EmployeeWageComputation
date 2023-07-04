f2fc4172ee0902cf197c411d7d20a6a978627639
ShreyasHakke1809 <shreyashakke5858@gmail.com>
2023-07-04 17:34:20 +0530

    Modifying program.cs file to add part time employee and wage

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            const int IS_PRESENT = 1;
+            const int FULL_TIME = 1;
+            const int PART_TIME = 2;
             const int EMP_RATE_PER_HR = 20;
             int empHrs = 0;
             int empWage = 0;
@@ -12,10 +13,15 @@
             Random random = new Random();
             int employeeInput=random.Next(0,2);
             Console.WriteLine("Random value :{0}", employeeInput);
-            if (employeeInput==IS_PRESENT) 
+            if (employeeInput == FULL_TIME) 
             {
                 Console.WriteLine("Employee is Present");
                 empHrs = 8;
+            }
+            else if(employeeInput == PART_TIME)
+                {
+                Console.WriteLine("FullTime Employee is Present");
+                empHrs = 4;
             }
             else
             {
