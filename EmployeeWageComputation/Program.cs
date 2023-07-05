3a311882657a04029e4e016c45a346e62cc7c56c
ShreyasHakke1809 <shreyashakke5858@gmail.com>
2023-07-05 19:43:01 +0530

    Modifying program.cs file to calulate wages of total working hours

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -10,14 +10,16 @@ namespace EmployeeWageComputation
             const int PART_TIME = 2;
             const int EMP_RATE_PER_HR = 20;
             const int NUMBER_OF_WORKING_DAYS = 20;
+            const int MAX_WORKING_DAYS = 100;
             int empHrs = 0;
             int empWage = 0;
             int totalWage = 0;
             int day = 1;
+            int totalWorkingHrs = 0;
 
             Console.WriteLine("Welcome To Employee Wage Computation");
             Random random = new Random();
-            for (day = 1; day <= NUMBER_OF_WORKING_DAYS; day++)
+            while (day <= NUMBER_OF_WORKING_DAYS && totalWorkingHrs <= MAX_WORKING_DAYS )
             {
                  int employeeInput = random.Next(0, 3);
                  switch (employeeInput)
@@ -34,9 +36,11 @@ namespace EmployeeWageComputation
                         //Console.WriteLine("Employee Is Absent");
                         break;
                 }
-            empWage = empHrs * EMP_RATE_PER_HR;
-            totalWage = totalWage + empWage;
-            Console.WriteLine("Day{0} employee wage is:{1}",day, empWage);
+                empWage = empHrs * EMP_RATE_PER_HR;
+                totalWage = totalWage + empWage;
+                Console.WriteLine("Day{0} employee wage is:{1}",day, empWage);
+                day ++;
+                totalWorkingHrs = totalWorkingHrs + empHrs;
 
             }
             Console.WriteLine("Total Wage for {0} days is:{1}", (day - 1), totalWage);
