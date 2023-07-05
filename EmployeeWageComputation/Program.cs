c6214f3ae09d861124513475a329b2e9085b8071
ShreyasHakke1809 <shreyashakke5858@gmail.com>
2023-07-05 19:01:52 +0530

    Modifying program.cs file to case statement

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace EmployeeWageComputation
+﻿using System.Linq.Expressions;
+
+namespace EmployeeWageComputation
 {
     internal class Program
     {
@@ -9,27 +11,28 @@
             const int EMP_RATE_PER_HR = 20;
             int empHrs = 0;
             int empWage = 0;
-            Console.WriteLine("Welcome To EmployeeWageComputation");
-            Random random = new Random();
-            int employeeInput=random.Next(0,2);
-            Console.WriteLine("Random value :{0}", employeeInput);
-            if (employeeInput == FULL_TIME) 
+
+            Console.WriteLine("Welcome To Employee Wage Computation");
+            Random random = new Random();   
+            int employeeInput = random.Next(0,3);
+            Console.WriteLine("Random value:{0}", employeeInput);
+            switch(employeeInput)
             {
-                Console.WriteLine("Employee is Present");
-                empHrs = 8;
-            }
-            else if(employeeInput == PART_TIME)
-                {
-                Console.WriteLine("FullTime Employee is Present");
-                empHrs = 4;
-            }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
+                case PART_TIME:
+                    Console.WriteLine("FullTime Employee Is Present");
+                    empHrs = 8;
+                    break;
+                case FULL_TIME:
+                    Console.WriteLine("PartTime Employee Is Present");
+                    empHrs = 4;
+                    break;
+                default:
+                    Console.WriteLine("Employee Is Absent");
+                    break;
             }
             empWage = empHrs * EMP_RATE_PER_HR;
-            Console.WriteLine("Daily employee wage is :{0}", empWage);
-            Console.ReadLine();
+                Console.WriteLine("Daily Employee Wage is:{0}", empWage);
+            
         }
     }
 }
