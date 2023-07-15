2dad584cee59a2f2a189b11293a7384f4fd9328a
ShreyasHakke1809 <shreyashakke5858@gmail.com>
2023-07-15 22:13:19 +0530

    Adding new program to the UC7 branch

---
+++ EmployeeWageComputation/EmployeeWage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    internal class EmployeeWage
+    {
+        const int FULL_TIME = 1;
+        const int PART_TIME = 2;
+        const int EMP_RATE_PER_HR = 20;
+        const int NUMBER_OF_WORKING_DAYS = 20;
+        const int MAX_WORKING_DAYS = 100;
+        public void ComputeWage()
+        {
+            int empHrs = 0;
+            int empWage = 0;
+            int totalWage = 0;
+            int day = 1;
+            int totalWorkingHrs = 0;
+            Console.WriteLine("Welcome To Employee Wage Computation");
+            Random random = new Random();
+            while (day <= NUMBER_OF_WORKING_DAYS && totalWorkingHrs <= MAX_WORKING_DAYS)
+            {
+                int employeeInput = random.Next(0, 3);
+                switch (employeeInput)
+                {
+                    case PART_TIME:
+                        empHrs = 8;
+                        break;
+                    case FULL_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HR;
+                totalWage = totalWage + empWage;
+                Console.WriteLine("Day{0} employee wage is:{1}", day, empWage);
+                day++;
+                totalWorkingHrs = totalWorkingHrs + empHrs;
+            }
+            Console.WriteLine("Total Wage for {0} days is:{1}", (day - 1), totalWage);
+        }
+    }
+}
--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -6,44 +6,8 @@ namespace EmployeeWageComputation
     {
         static void Main(string[] args)
         {
-            const int FULL_TIME = 1;
-            const int PART_TIME = 2;
-            const int EMP_RATE_PER_HR = 20;
-            const int NUMBER_OF_WORKING_DAYS = 20;
-            const int MAX_WORKING_DAYS = 100;
-            int empHrs = 0;
-            int empWage = 0;
-            int totalWage = 0;
-            int day = 1;
-            int totalWorkingHrs = 0;
-
-            Console.WriteLine("Welcome To Employee Wage Computation");
-            Random random = new Random();
-            while (day <= NUMBER_OF_WORKING_DAYS && totalWorkingHrs <= MAX_WORKING_DAYS )
-            {
-                 int employeeInput = random.Next(0, 3);
-                 switch (employeeInput)
-                {
-                    case PART_TIME:
-                       // Console.WriteLine("FullTime Employee Is Present");
-                        empHrs = 8;
-                        break;
-                    case FULL_TIME:
-                        //Console.WriteLine("PartTime Employee Is Present");
-                        empHrs = 4;
-                        break;
-                    default:
-                        //Console.WriteLine("Employee Is Absent");
-                        break;
-                }
-                empWage = empHrs * EMP_RATE_PER_HR;
-                totalWage = totalWage + empWage;
-                Console.WriteLine("Day{0} employee wage is:{1}",day, empWage);
-                day ++;
-                totalWorkingHrs = totalWorkingHrs + empHrs;
-
-            }
-            Console.WriteLine("Total Wage for {0} days is:{1}", (day - 1), totalWage);
+            EmployeeWage employeeWage = new EmployeeWage(); 
+            employeeWage.ComputeWage(); 
         }
-    }
+    }    
 }
